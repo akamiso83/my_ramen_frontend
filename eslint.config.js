ee80a7dacbc3cb47766f117648874bbb7aadbb6b
akamiso83 <akamiso8383@outlook.jp>
2025-07-01 02:06:36 +0900

    linterおよびformaterの組み込み

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,18 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
 
 export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -19,5 +23,9 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      ...react.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+    },
   },
-])
+]);
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+// フォーマッターの定義ファイル必要に応じて以下の内容を編集すること
+export default {
+          semi: true,
+          singleQuote: true,
+          trailingComma: 'all',
+          printWidth: 100,
+          tabWidth: 2,
+};
